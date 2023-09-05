f661dac2294ae18b452409254133d22d1a2754a3
Kainan <kainan.bradachi@flashnetbrasil.com.br>
2023-09-05 20:51:27 -0300

    Upload dos Arquivos

---
+++ Praticando 10 - Hello Laravel/resources/views/inicio.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Início</h1>
+    <hr>
+    <p>Página de acesso ao "hello world" Laravel</p>
+    <ul>
+        <li>
+            <a href="/welcome">welcome</a>
+        </li>
+    </ul>
+    <p>Página que recebe um parâmetro qualquer</p>
+    <ul>
+        <li>
+            <a href="/pg2">página 2</a>
+        </li>
+    </ul>
+</body>
+</html>
